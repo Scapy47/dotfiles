ca7bd92434beeb11a63e2e9968a2299415521368
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-06-25 10:55:59 +0530

    Update alias.sh

--- .config/env/alias.sh
+++ .config/env/alias.sh
@@ -1,2 +1,2 @@
 # this makes standard git repo works as bare repo so it will not modifey or delete other files and only ones that are being added with add arg
-alias dotfiles='git --git-dir=$HOME/.dotfiles/.git/ --work-tree=$HOME'
+alias dotfiles='git --git-dir="${$HOME/.dotfiles/.git/:-$HOME/.dotfiles}" --work-tree=$HOME'
